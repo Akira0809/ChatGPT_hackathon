107958c3044566ab0c8fa5accf93bdda83592607
Akira0809 <akira_0809@icloud.com>
2023-08-11 23:53:13 +0900

    JSの追記

--- game/views.py
+++ game/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
 from .models import Data
 import openai
+import json
 
 # Create your views here.
 def game(request):
     data= Data.objects.order_by("-created_at").filter()
-    questions = []
-    answer = []
-    hints = []
-    commentary = []
-    for i in range(5):
-        questions.append(data[i]["question"])
-        answer.append(data[i]["answer"])
-        hints.append(data[i]["hints"])
-        commentary.append(data[i]["commentary"])
-    return render(request, "base.html")
+    data = json.loads(data)
+    return render(request, "base.html", context={"data": data})
 
 def difficulty(request):
     data = Data.objects.all()

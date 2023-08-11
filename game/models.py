185170c0fc3fd74d3dc195dd3d1212132b1e92b8
Akira0809 <akira_0809@icloud.com>
2023-08-11 12:48:28 +0900

    プロンプト追加

--- game/models.py
+++ game/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Data(models.Model):
     questions = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True) #データを追加した時間

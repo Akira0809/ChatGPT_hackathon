290819498b6ca7f2a4a70a411631e35b2ea37eae
Akira0809 <akira_0809@icloud.com>
2023-08-13 17:47:04 +0900

    リザルト画面の実装

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,6 @@ from . import views, views_difficulty
 
 app_name = 'game'
 urlpatterns = [
-    path("", views.game, name="game"),
     path("difficulty/", views_difficulty.difficulty, name="difficulty"),
+    path("result/", views.result, name="result"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from .models import Data
-import json
 
-# Create your views here.
-def game(request):
-    data= Data.objects.get(id=19)
-    data = json.loads(data)
-    print(data)
-    return render(request, "base.html", context={"data": data})
+def result(request):
+    point = 1
+    return render(request, "result.html", context={"point": point})
